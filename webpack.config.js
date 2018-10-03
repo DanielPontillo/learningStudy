15aa06f04ae24880a16d2b170565b5e3b4560e76
DanielPontillo <dfp7615@gmail.com>
2018-10-03 16:38:47 +0200

    added webpack and fixed errors

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  module: {
+    rules: [ { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader', options: {
+          presets: ['react']
+        }}, 
+    { test: /\.css$/, use: ['style-loader','css-loader'] }, 
+    { test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' } ]
+    
+  }
+};
